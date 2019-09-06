39e38fa54176641a0e09945e17bfadf8fbe97c75
ninjawarrior1337 <teelar1337@gmail.com>
2019-09-06 16:42:24 -0700

    add profiling food

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -13,7 +13,27 @@
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+auto chassis = ChassisControllerFactory::create(
+	2,
+	-1,
+	AbstractMotor::gearset::blue,
+	{2_in, 12_in}
+);
+
+auto profileController = AsyncControllerFactory::motionProfile(
+  10.0,  // Maximum linear velocity of the Chassis in m/s
+  2.0,  // Maximum linear acceleration of the Chassis in m/s/s
+  10.0, // Maximum linear jerk of the Chassis in m/s/s/s
+  chassis // Chassis Controller
+);
+
 void opcontrol() {
+	profileController.generatePath({
+		Point{0_ft, 0_ft, 0_deg},  // Profile starting position, this will normally be (0, 0, 0)
+		Point{2_ft, -2_ft, 0_deg}}, // The next point in the profile, 3 feet forward
+		"A" // Profile name
+	);
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::Motor right_mtr(1);
 	pros::Motor left_mtr(2);
@@ -25,11 +45,13 @@ void opcontrol() {
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
 		left_mtr = left;
-		right_mtr = -right;
+		right_mtr = right;
 
 		if(master.get_digital(DIGITAL_A))
 		{
-			autonomous();
+			// autonomous();
+			profileController.setTarget("A");
+			profileController.waitUntilSettled();
 		}
 
 		pros::lcd::set_text(3, std::to_string(-right_mtr.get_position()));
